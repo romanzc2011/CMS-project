cf0570e10e9d4849f4fa72ceee6ab403d45d7e83
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2022-06-21 16:36:49 -0500

    Create calculateShipping.php

---
+++ public/calculateShipping.php
@@ -0,0 +1,58 @@
+<?php
+
+class Product {
+    private $price;
+    private $weight;
+    private $freeShipping = false;
+
+    public function __construct($price, $weight){
+        $this->weight = $weight;
+        $this->price = $price;
+    }
+
+    function getWeight(){
+        return $this->weight;
+    }
+
+    function setFreeShipping(){
+        $this->freeShipping = true;
+    }
+}
+
+class Shipping {
+    private $totalShipping;
+    private $products;
+    private $pricePerKilogram;
+
+    public function __construct(){
+
+    }
+
+    public function addProducts(Product $product){
+        $this->products[] = $product;
+    }
+
+    public function calculateTotalShipping(){
+        foreach($this->products as $product){
+            if(!$product->getFfreeShipping()) {
+                $this->totalShipping += $product->getWeight() * $this->pricePerKilogram;
+            }
+        }
+    }
+
+    public function getTotalShippingPrice(){
+        return $this->totalShipping;
+    }
+}
+
+$product = new Product(5, 1);
+
+$pricePerKilogram = 5;
+
+$shipping = new Shipping($pricePerKilogram);
+
+$shipping->addProducts($product);
+$shipping->calculateTotalShipping();
+$totalShippingPrice = $shipping->calculateTotalShipping();
+var_dump($totalShippingPrice);
+
