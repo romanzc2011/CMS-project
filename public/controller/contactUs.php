e8412479898437549c85876031feb21ceb9998b9
romanzc2011 <93228839+romanzc2011@users.noreply.github.com>
2022-06-22 16:55:14 -0500

    newupdate

--- public/controller/contactUs.php
+++ public/controller/contactUs.php
@@ -1,2 +1,15 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/php/CMS-project/public/view/contact-us.html';
+
+class contactController {
+    function showFormAction(){
+        include $_SERVER['DOCUMENT_ROOT'] . '/php/CMS-project/public/view/contact-us.html';
+    }
+
+    function submitContactFormAction(){
+        // validate
+        //store data
+        //send email
+
+        include $_SERVER['DOCUMENT_ROOT'] . '/php/CMS-project/public/view/contact-us-thank-you.html';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,26 @@
 <?php
 
-$section = isset($_GET['section']) ? $_GET['section'] : 'home';
+$section = $_GET['section'] ?? $_POST['section'] ?? 'home';
+$action = $_GET['action'] ?? $_POST['action'] ?? 'show';
+
+if(isset($_GET['section'])){
+    $section = $_GET['section'];
+}elseif(isset($_POST[section])){
+    $section = $_POST['section'];
+}else{
+    $section = 'home';
+}
 
 if($section  == 'about-us'){
     include 'controller/aboutUsPage.php';
 }else if($section == 'contact-us'){
     include 'controller/contactUs.php';
-}else{
-    include 'controller/homePage.php';
+    $contactController = new ContactController();
+
+if($action == 'show'){} elseif($action == 'submit' {
+
 }
+
 ?>
 
 
